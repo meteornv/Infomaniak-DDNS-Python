0ab3140f85ab2b60bcbfd6ece177901015684408
meteornv <47923266+meteornv@users.noreply.github.com>
2024-02-11 18:44:45 +0100

    Update request.py

--- request.py
+++ request.py
@@ -1,15 +1,32 @@
 import requests
-subdomain=input("Enter your Subdomain (leave blank if none):") #this is your subdomain
-domain=input("Enter your domain:") #this is your main domain
-username=input("Enter your DDNS username:")#this is your dynamic DNS username
-password=input("Enter your DDNS password:") #this is your dynamic DNS password
-if subdomain=="":
-    API_ENDPOINT = "https://infomaniak.com/nic/update?hostname="+domain+"&username="+username+"&password="+password
-else:
-    API_ENDPOINT = "https://infomaniak.com/nic/update?hostname="+subdomain+"."+domain+"&username="+username+"&password="+password
+from pushbullet import Pushbullet
+import time
+API_KEY=""
+pb = Pushbullet(API_KEY)
 
-print(API_ENDPOINT)
-r = requests.post(url = API_ENDPOINT) 
-  
-pastebin_url = r.text 
-print("The pastebin URL is:%s"%pastebin_url) 
+def refresh_IP(domain, retry=3):
+    api_endpoint = "https://infomaniak.com/nic/update?hostname="+domain+"&username=username&password=password"
+    r = requests.post(url = api_endpoint) 
+    pastebin_url = r.text
+    if pastebin_url.startswith("nochg"):
+        print(" No change for ",domain," The pastebin URL is:%s"%pastebin_url)
+    elif pastebin_url.startswith("no_change"):
+        print(" No change for ",domain," The pastebin URL is:%s"%pastebin_url)
+    elif pastebin_url.startswith("good"):
+        ip=pastebin_url.replace("good ",'')
+        push = pb.push_note("Changement d'IP", "L'IP de "+domain+" est maintenant "+ip)
+        print("Good for ",domain," The pastebin URL is:%s"%pastebin_url)
+    else:
+        if retry > 1:
+            print("Retrying for ", domain," ...:",pastebin_url)
+            time.sleep(600)
+            refresh_IP(domain, retry=retry - 1)
+        else:
+            push = pb.push_note("Changement d'IP","Un problème est survenu lors du check de changement d'IP pour le domaine "+domain+" avec le message suivant: " + pastebin_url)
+            print("Error. The pastebin URL is: %s" % pastebin_url)
+
+
+domains=[""]
+for x in domains:
+    refresh_IP(x)
+
